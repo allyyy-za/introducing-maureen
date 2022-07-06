af61ee2fb75d25e37d201a8c5f0047dfda7b8817
Allyza Maureen Catura <amaureencatura@gmail.com>
2022-07-06 17:27:33 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {},
+    colors: {
+      'main': '#FF566E',
+      'background': '#f9fafb',
+      'gray': '#F3F3F3',
+      'dark': '#000000',
+    }
+  },
+  plugins: [],
+}
