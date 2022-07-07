a7e11aea4f913629c5fde5d28f3b6f9bc34f5309
Allyza Maureen Catura <amaureencatura@gmail.com>
2022-07-07 11:23:01 +0800

    Added responsiveness to the web page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,21 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
-    colors: {
-      'main': '#FF566E',
-      'background': '#f9fafb',
-      'gray': '#F3F3F3',
-      'dark': '#000000',
-    }
+    extend: {
+      colors: {
+        'main': '#FF566E',
+        'background': '#f9fafb',
+        'gray': '#F3F3F3',
+        'dark': '#000000',
+        'secondary': "#FF7B8E",
+        'tertiary': "#FF2544",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+    },
   },
   plugins: [],
 }
